9f7545f3153fd70d6c3b531038a241d599652eb0
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2022-12-01 21:19:52 +0100

    [ADD] the home page with the header and the styles sheets

--- front/src/index.js
+++ front/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './styles/main.css';
+import Home from './pages/home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ front/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React, {Fragment} from "react";
+import Header from "../components/header";
+
+function Home () {
+  return (
+    <Fragment>
+      <Header />
+
+    </Fragment>
+  );
+}
+
+export default Home;
