e62fc3bec529c553f170dbc69e197b95e32a9ebf
Ing-Tek <kitae.seo@epitech.eu>
2022-12-01 23:37:02 +0100

    button move, unclickable

--- front/src/components/Jouer.jsx
+++ front/src/components/Jouer.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 
 function Jouer() {
-  return (
-    <div>
-        <button
-            id="jouer"
-            onClick={() => {
-                window.location.href = "/jeu";
-            }}
-        >
-            Jouer
-        </button>
-    </div>
-  );
+    return (
+        <div>
+            <button
+                id="jouer"
+                onClick={() => {
+                    window.location.href = "/jeu";
+                }}
+
+                onMouseEnter={() => {
+                    const btn = document.getElementById('jouer');
+                    btn.style.left = Math.floor(Math.random() * 300 - 100) + 'px';
+                    btn.style.top = Math.floor(Math.random() * 600 + 100) + 'px';
+                    btn.disabled = true;
+                }}
+            >
+                Jouer
+            </button>
+        </div>
+    );
 }
 
 export default Jouer;
