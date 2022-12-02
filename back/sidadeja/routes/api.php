6531d58b93f603a8767dbf02b6a84c2fe37de5a7
Quentin L <quentin.le-noir@epitech.eu>
2022-12-02 05:05:27 +0100

    [FIX] bug de l'import API en api

--- back/sidadeja/routes/api.php
+++ back/sidadeja/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\questionController;
+use App\Http\Controllers\api\questionController;
 
 /*
 |--------------------------------------------------------------------------
