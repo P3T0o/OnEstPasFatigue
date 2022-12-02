1fe330bb9f46448759db01293d8765e42c3dd0f1
Quentin L <quentin.le-noir@epitech.eu>
2022-12-02 01:57:48 +0100

    [ADD] Footer

---
+++ front/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <div className={"footer"}>
+            de
+        </div>
+    );
+}
+
+export default Footer;
--- front/src/pages/home.jsx
+++ front/src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React, {Fragment} from "react";
 import Header from "../components/header";
 import HomeInput from "../components/homeInput";
+import Footer from "../components/footer";
 
 function Home () {
   return (
     <Fragment>
         <Header />
         <HomeInput />
+        <Footer />
     </Fragment>
   );
 }
