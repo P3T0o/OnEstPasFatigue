dae12b247407109491da45fb053ce40705bf9886
Quentin L <quentin.le-noir@epitech.eu>
2022-12-02 02:00:15 +0100

    [ADD] footer sur la page question

--- front/src/pages/question.jsx
+++ front/src/pages/question.jsx
@@ -1,12 +1,14 @@
 import React, {Fragment} from "react";
 import Header from "../components/header";
 import Questions from "../components/questions";
+import Footer from "../components/footer";
 
 function Question () {
   return (
     <Fragment>
         <Header />
         <Questions />
+        <Footer />
     </Fragment>
   );
 }
