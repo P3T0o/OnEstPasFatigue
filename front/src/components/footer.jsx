b9dec2dfaa8ae925aa6805d30558383f148b5110
Quentin L <quentin.le-noir@epitech.eu>
2022-12-02 02:56:54 +0100

    [ADD] Footer fini

--- front/src/components/footer.jsx
+++ front/src/components/footer.jsx
@@ -3,12 +3,22 @@ import React from "react";
 function Footer () {
     return (
         <div className={"footer"}>
-            <div className={"footer_left"}>
-                <span className={"footer_equipe"}>Equipe</span>
+            <h4>Développeurs</h4>
+            <div className={"box"}>
+                <div className={"footer_left"}>
+                    <ul className={"footer_ul"}>
+                        <li className={"footer_li"}>Kitae Seo</li>
+                        <li className={"footer_li"}>Elouan Da-Costa Peixoto</li>
+                    </ul>
+                </div>
+                <div className={"footer_right"}>
+                    <ul className={"footer_ul"}>
+                        <li className={"footer_li"}>Erwan Geslin</li>
+                        <li className={"footer_li"}>Quentin Le Noir</li>
+                    </ul>
+                </div> 
             </div>
-            <div className={"footer_right"}>
-
-            </div>
+            
         </div>
     );
 }
