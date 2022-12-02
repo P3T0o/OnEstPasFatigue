95674590e3439500e3fb619c7411cfa9a3b003a5
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2022-12-02 05:35:19 +0100

    ADD: Page 404 + Pictures: 404ERROR - STORM - TINDER -LOGO

--- back/sidadeja/resources/views/errors/404.blade.php
+++ back/sidadeja/resources/views/errors/404.blade.php
@@ -1,4 +1,21 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="/css/404.css">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/404.css">
+    <title>404</title>
+</head> 
 
-{{-- ICI TU MET TON CODE --}}
+<body>
+    <div class = "img404"></div>
+    <div class = "imgLoop"></div>
+    <div class = "imgLogo"></div>
+    <div class = "imgtider"></div>
+    <div class = "imgLoop"></div>
+    <div class = "imgtider"></div>
+</body>
+
+<!-- {{-- ICI TU MET TON CODE --}} -->
+
